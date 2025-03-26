0a957a64048a3789e09b5f2d640110e8e67e774d
Muhamad Fitrayuda <t-muhamad.fitrayuda@traveloka.com>
2025-03-26 07:21:42 +0700

    feature : folder structure

---
+++ src/main/java/com/gotrash/GotrashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gotrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotrashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotrashBackendApplication.class, args);
+	}
+
+}
