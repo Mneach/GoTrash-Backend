2eed77cf5cc0bb1a98876153543df1e8f59ac2b1
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-05-30 07:03:03 +0700

    feature : leaderboard

--- src/main/java/com/gotrash/repository/CitizenRepository.java
+++ src/main/java/com/gotrash/repository/CitizenRepository.java
@@ -3,12 +3,17 @@ package com.gotrash.repository;
 import com.gotrash.entity.CitizenEntity;
 import com.gotrash.entity.WasteBankEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigInteger;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CitizenRepository extends JpaRepository<CitizenEntity, UUID> {
   Optional<CitizenEntity> findByUser_UserId(UUID userId);
   Optional<CitizenEntity> findByPhoneNumber(String phoneNumber);
+
+  @Query("SELECT c FROM CitizenEntity c ORDER BY c.rating DESC LIMIT 10")
+  List<CitizenEntity> findTop10ByOrderByRatingDesc();
 }
